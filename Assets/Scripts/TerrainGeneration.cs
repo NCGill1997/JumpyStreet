1b63e0e2e3064ebd8d0032f44bc389150f91a46e
William C <williamcroney64@gmail.com>
2020-09-30 00:31:16 -0400

    Coding Generation.
    Basic Generation Code has been put in it's script. Testing on it working needs to be done but otherwise it's there.

--- Assets/Scripts/TerrainGeneration.cs
+++ Assets/Scripts/TerrainGeneration.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class TerrainGeneration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Grass, Water, Road; //Our Different Terrain Types
 
-    // Update is called once per frame
+    int firstRand, secondRand, disPlayer;
+
+    Vector3 intPos = new Vector3(0,0,0);
+
     void Update()
     {
-        
+        if (Input.GetButton ("Up"))
+        {
+            firstRand = Random.Range(1, 4);
+            if (firstRand == 1) // Generation of Grass
+            {
+                secondRand = Random.Range(1, 8); //Note to self - Ajust this later. This affects how many common this tpye of tile is. - William
+                for (int i= 0; i < secondRand; i++)
+                {
+                    intPos = new Vector3(0, -.1f, disPlayer);
+                    disPlayer += 1;
+                    GameObject GrassInt = Instantiate(Grass) as GameObject;
+                    GrassInt.transform.position = intPos;
+                }
+            }
+
+            if (firstRand == 2) //Generation of Road
+            {
+                secondRand = Random.Range(1, 8); //Note to self - Ajust this later. This affects how many common this tpye of tile is. - William
+                for (int i = 0; i < secondRand; i++)
+                {
+                    intPos = new Vector3(0, -.2f, disPlayer);
+                    disPlayer += 1;
+                    GameObject RoadInt = Instantiate(Road) as GameObject;
+                    RoadInt.transform.position = intPos;
+                }
+            }
+
+            if (firstRand == 3) //Generation of the water
+            {
+                secondRand = Random.Range(1, 8); //Note to self - Ajust this later. This affects how many common this tpye of tile is. - William
+                for (int i = 0; i < secondRand; i++)
+                {
+                    intPos = new Vector3(0, 0, disPlayer);
+                    disPlayer += 1;
+                    GameObject WaterInt = Instantiate(Water) as GameObject;
+                    WaterInt.transform.position = intPos;
+                }
+            }
+        }
     }
 }
