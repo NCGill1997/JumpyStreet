5293b3309e9d4b0680c46fdb37eb23ed3d105512
NCGill1997 <71213196+NCGill1997@users.noreply.github.com>
2020-09-30 01:28:55 -0400

    Generation, Some prefab work
    Generation, Asset prefab adjustment, and Car prefab creation.  Collisions still not working :c

---
+++ Assets/Scripts/NCG Scripts/CarGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarGenerator : MonoBehaviour
+{
+    
+    public GameObject[] carPrefabs = new GameObject[4];
+    private GameObject[] carsInPlay = new GameObject[3];
+    public GameObject[] lanes = new GameObject[3];
+    //public GameObject[] laneEnds = new GameObject[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LaneSpawning1());
+        StartCoroutine(LaneSpawning2());
+        StartCoroutine(LaneSpawning3());
+
+    }
+
+    //Spawning rates are hard-coded for now.  These can be easily randomized, but for testing purposes it was easier to just use hard-coded
+    //times.  Each of these 
+
+    private IEnumerator LaneSpawning1()
+    {
+        int car = Random.Range(0, 4);
+        
+        yield return new WaitForSeconds(3);
+        carsInPlay[0] = Instantiate(carPrefabs[car], lanes[0].transform.position, lanes[0].transform.rotation);
+        
+        StartCoroutine(LaneSpawning1());
+
+        /*for (int i = 0; i < 3; i++)
+        {
+            int car = Random.Range(0, 4);
+            yield return new WaitForSeconds(i);
+            carsInPlay[i] = Instantiate(carPrefabs[car], lanes[i].transform.position, lanes[i].transform.rotation);
+        }*/  //This is an attempt to make the code a little better.  Will re-visit this later on.
+        //StartCoroutine(LaneSpawning1());
+    }
+
+    private IEnumerator LaneSpawning2()
+    {
+        int car = Random.Range(0, 4);
+        yield return new WaitForSeconds(2);
+        carsInPlay[1] = Instantiate(carPrefabs[car], lanes[1].transform.position, lanes[0].transform.rotation);
+        
+        StartCoroutine(LaneSpawning2());
+    }
+
+    private IEnumerator LaneSpawning3()
+    {
+        int car = Random.Range(0, 4);
+        yield return new WaitForSeconds(1);
+        carsInPlay[2] = Instantiate(carPrefabs[car], lanes[2].transform.position, lanes[0].transform.rotation);
+        
+        StartCoroutine(LaneSpawning3());
+    }
+}
---
+++ Assets/Scripts/NCG Scripts/CarScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        StartCoroutine(DestroySelf());
+    }
+
+    private void Update() //Movement for the car.
+    {
+        transform.Translate(new Vector3(0, -Time.deltaTime * 3, 0));
+    }
+
+    //Check collision for the collider that destroys the cars, and the collider of the player.
+    public void OnColliderEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Destroy")
+        {
+            Debug.Log("Destroy");
+            Destroy(gameObject);
+        }
+        //This was not working, for some reason.  
+        //The collider, whether a trigger or not, was refusing to call this method.
+        //Went with plan B for now.
+
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player hit " + this.gameObject.name); //Placeholder.  Will affect the player further on
+        }
+    }
+
+
+    IEnumerator DestroySelf()  //This was the "Plan B" for the destruction of the cars.
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("Destroy" + this.gameObject.name);
+        Destroy(gameObject);
+    }
+}
